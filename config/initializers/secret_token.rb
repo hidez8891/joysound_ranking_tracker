e35ef010a7d78fae0d8baa74aa35c0e2540e0413
hidez8891 <minowith+github@gmail.com>
2012-10-02 14:35:01 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JoysoundRankingWatcher::Application.config.secret_token = '3c564c9495bbc5538ae78fb98c4d4bad67b055df315089c22377e90cb2a01600d8d15a8367f65cd0ef392ae7b0c995491edd5a09543081a2b65763d45ac533c0'
